e227a008f9fa09552795f5a2e7245123550c2cd6
broadwise.cxo@gmail.com <broadwise.cxo@gmail.com>
2020-04-09 02:37:43 +0100

    initial commit

---
+++ src/main/java/com/marketwinks/marketsignals/MarketsignalsApplication.java
@@ -0,0 +1,13 @@
+package com.marketwinks.marketsignals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketsignalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketsignalsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marketwinks/marketsignals/MarketsignalsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marketwinks.marketsignals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketsignalsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
